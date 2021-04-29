39ac1592827f1050b6573a9f5a3819142eb3dad5
esanchep <esanchezp85@gmail.com>
2021-04-29 22:47:11 +0200

    Clean code

--- src/main/java/com/clearpay/reactive/controller/WalletController.java
+++ src/main/java/com/clearpay/reactive/controller/WalletController.java
@@ -1,19 +1,15 @@
 package com.clearpay.reactive.controller;
 
-import com.clearpay.reactive.domain.Response;
-import com.clearpay.reactive.domain.Wallet;
-import com.clearpay.reactive.service.WalletService;
 import com.clearpay.reactive.controller.utils.ControllerUtils;
+import com.clearpay.reactive.domain.Response;
+import com.clearpay.reactive.service.WalletService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 import static com.clearpay.reactive.controller.utils.ControllerConstants.OPERATION_SUCCESS;
 import static org.springframework.http.HttpStatus.OK;
@@ -28,7 +24,7 @@ public class WalletController {
     private WalletService walletService;
 
     @GetMapping()
-    private Mono<ResponseEntity<Response<List<Wallet>>>> getWalletsByUserId(@PathVariable() String userId) {
+    Mono getWalletsByUserId(@PathVariable() String userId) {
         log.info("Get wallets from user with ID {} request received", userId);
         return walletService.getWalletsByUserId(userId)
                 .collectList()
--- src/main/java/com/clearpay/reactive/controller/utils/ControllerUtils.java
+++ src/main/java/com/clearpay/reactive/controller/utils/ControllerUtils.java
@@ -4,13 +4,20 @@ import com.clearpay.reactive.domain.Response;
 import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.ResponseEntity.badRequest;
 import static org.springframework.http.ResponseEntity.ok;
+import static reactor.core.publisher.Mono.just;
 
 public class ControllerUtils {
 
     public static <T> Mono<ResponseEntity<Response<T>>> getMonoResponseOnError(Throwable throwable) {
-        return Mono.just(ok().body(new Response<>(INTERNAL_SERVER_ERROR.value(), throwable.getMessage(), null)));
+        return just(ok().body(new Response<>(INTERNAL_SERVER_ERROR.value(), throwable.getMessage(), null)));
+    }
+
+    public static Mono buildBadRequestResponse(String message) {
+        return just(badRequest().body(new Response<>(BAD_REQUEST.value(), message, null)));
     }
 
 }
